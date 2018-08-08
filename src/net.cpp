98db50e8f1530d822bc4ed6679a3f99bdd5794b1
oshek14 <oshek14@freeuni.edu.ge>
2018-08-08 20:55:14 +0400

    good

--- src/net.cpp
+++ src/net.cpp
@@ -1173,12 +1173,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"learncointools.com", "206.189.227.174"},
+    {"learncointools.com", "138.197.221.83"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"learncointools.com", "206.189.227.174"},
+    {"learncointools.com", "138.197.221.83"},
     {NULL, NULL}
 };
 
